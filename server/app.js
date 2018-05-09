0a9e0fc6d1c41445f6c32a0ae12ff23f633c46f3
Gabriel Cebrián <kunry3@hotmail.com>
2018-05-09 15:37:12 +0200

    Go

--- server/app.js
+++ server/app.js
@@ -95,6 +95,6 @@ const shopRoutes = require("./routes/shop");
 app.use("/shop", shopRoutes)
 
 const messegeRoutes = require("./routes/message");
-app.use.("/message", meesegeRoutes);
+app.use("/message", meesegeRoutes);
 
 module.exports = app;
