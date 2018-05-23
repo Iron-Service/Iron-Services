78f3e6f26b74e5691e5bf2978dc0424f1b91f082
muflonex <muflonex@gmail.com>
2018-05-23 12:30:18 +0200

    icons

--- client/src/app/services/comment.service.ts
+++ client/src/app/services/comment.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class CommentService {
+  BASE_URL = "http://localhost:3000";
+  options: Object = { withCredentials: true };
+constructor(private http: Http) { }
 
-constructor() { }
-
+getList() {
+  return this.http
+    .get(`${environment.BASE_URL}/api/comment`, this.options)
+    .map(res => res.json());
+}
 }
